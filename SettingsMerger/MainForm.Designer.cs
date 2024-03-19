88ed7fe5c87d6e0a7703fcbbd99cd3b44308e7bb
Lenche <lencegeorgieva@yahoo.com>
2024-03-19 17:47:23 +0200

    styling changes (#4)
    * display change
    
    * fix some buttons
    
    * display changes
    
    ---------
    
    Co-authored-by: Lenche Petrov <Lenche.Petrov@wolterskluwer.com>
    Co-authored-by: Ivan Stefanov <ivan.stefanov@scalefocus.com>

--- SettingsMerger/MainForm.Designer.cs
+++ SettingsMerger/MainForm.Designer.cs
@@ -58,6 +58,7 @@
             gbDestination = new GroupBox();
             groupBox1 = new GroupBox();
             gbOutput = new GroupBox();
+            groupBox2 = new GroupBox();
             toolStrip1.SuspendLayout();
             tcInput.SuspendLayout();
             tpAzure.SuspendLayout();
@@ -66,15 +67,16 @@
             gbDestination.SuspendLayout();
             groupBox1.SuspendLayout();
             gbOutput.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label1.AutoSize = true;
-            label1.Location = new Point(8, 15);
+            label1.Location = new Point(9, 20);
             label1.Name = "label1";
-            label1.Size = new Size(103, 15);
+            label1.Size = new Size(131, 20);
             label1.TabIndex = 0;
             label1.Text = "Exported file path:";
             // 
@@ -82,10 +84,10 @@
             // 
             label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label2.AutoSize = true;
-            label2.Location = new Point(6, 19);
-            label2.Margin = new Padding(3);
+            label2.Location = new Point(7, 25);
+            label2.Margin = new Padding(3, 4, 3, 4);
             label2.Name = "label2";
-            label2.Size = new Size(115, 15);
+            label2.Size = new Size(146, 20);
             label2.TabIndex = 1;
             label2.Text = "Dev setting file path:";
             // 
@@ -93,34 +95,37 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(12, 31);
+            label3.Location = new Point(14, 41);
             label3.Name = "label3";
-            label3.Size = new Size(261, 45);
+            label3.Size = new Size(327, 54);
             label3.TabIndex = 2;
             label3.Text = "Settings Merger";
             // 
             // tbxExportedSettingsFilePath
             // 
             tbxExportedSettingsFilePath.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            tbxExportedSettingsFilePath.Location = new Point(8, 33);
+            tbxExportedSettingsFilePath.Location = new Point(9, 44);
+            tbxExportedSettingsFilePath.Margin = new Padding(3, 4, 3, 4);
             tbxExportedSettingsFilePath.Name = "tbxExportedSettingsFilePath";
-            tbxExportedSettingsFilePath.Size = new Size(415, 23);
+            tbxExportedSettingsFilePath.Size = new Size(474, 27);
             tbxExportedSettingsFilePath.TabIndex = 3;
             // 
             // tbxLocalSettingsFilePath
             // 
             tbxLocalSettingsFilePath.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            tbxLocalSettingsFilePath.Location = new Point(6, 40);
+            tbxLocalSettingsFilePath.Location = new Point(7, 53);
+            tbxLocalSettingsFilePath.Margin = new Padding(3, 4, 3, 4);
             tbxLocalSettingsFilePath.Name = "tbxLocalSettingsFilePath";
-            tbxLocalSettingsFilePath.Size = new Size(357, 23);
+            tbxLocalSettingsFilePath.Size = new Size(407, 27);
             tbxLocalSettingsFilePath.TabIndex = 4;
             // 
             // btnBrowseExportedSettings
             // 
             btnBrowseExportedSettings.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            btnBrowseExportedSettings.Location = new Point(429, 33);
+            btnBrowseExportedSettings.Location = new Point(490, 44);
+            btnBrowseExportedSettings.Margin = new Padding(3, 4, 3, 4);
             btnBrowseExportedSettings.Name = "btnBrowseExportedSettings";
-            btnBrowseExportedSettings.Size = new Size(75, 23);
+            btnBrowseExportedSettings.Size = new Size(88, 31);
             btnBrowseExportedSettings.TabIndex = 5;
             btnBrowseExportedSettings.Text = "Browse";
             btnBrowseExportedSettings.UseVisualStyleBackColor = true;
@@ -129,9 +134,10 @@
             // btnBrowseLocalSettings
             // 
             btnBrowseLocalSettings.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            btnBrowseLocalSettings.Location = new Point(369, 40);
+            btnBrowseLocalSettings.Location = new Point(418, 53);
+            btnBrowseLocalSettings.Margin = new Padding(3, 4, 3, 4);
             btnBrowseLocalSettings.Name = "btnBrowseLocalSettings";
-            btnBrowseLocalSettings.Size = new Size(75, 23);
+            btnBrowseLocalSettings.Size = new Size(85, 31);
             btnBrowseLocalSettings.TabIndex = 6;
             btnBrowseLocalSettings.Text = "Browse";
             btnBrowseLocalSettings.UseVisualStyleBackColor = true;
@@ -147,9 +153,10 @@
             btnMerge.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btnMerge.BackColor = Color.LightCoral;
             btnMerge.ForeColor = SystemColors.ControlText;
-            btnMerge.Location = new Point(1034, 116);
+            btnMerge.Location = new Point(7, 81);
+            btnMerge.Margin = new Padding(3, 4, 3, 4);
             btnMerge.Name = "btnMerge";
-            btnMerge.Size = new Size(75, 72);
+            btnMerge.Size = new Size(117, 43);
             btnMerge.TabIndex = 9;
             btnMerge.Text = "Merge";
             btnMerge.UseVisualStyleBackColor = false;
@@ -158,19 +165,21 @@
             // tbxOutput
             // 
             tbxOutput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            tbxOutput.Location = new Point(6, 22);
+            tbxOutput.Location = new Point(7, 29);
+            tbxOutput.Margin = new Padding(3, 4, 3, 4);
             tbxOutput.Multiline = true;
             tbxOutput.Name = "tbxOutput";
             tbxOutput.ScrollBars = ScrollBars.Both;
-            tbxOutput.Size = new Size(1089, 473);
+            tbxOutput.Size = new Size(1244, 629);
             tbxOutput.TabIndex = 10;
             // 
             // rbLocalSettings
             // 
             rbLocalSettings.AutoSize = true;
-            rbLocalSettings.Location = new Point(7, 47);
+            rbLocalSettings.Location = new Point(8, 63);
+            rbLocalSettings.Margin = new Padding(3, 4, 3, 4);
             rbLocalSettings.Name = "rbLocalSettings";
-            rbLocalSettings.Size = new Size(119, 19);
+            rbLocalSettings.Size = new Size(146, 24);
             rbLocalSettings.TabIndex = 12;
             rbLocalSettings.Text = "local.settings.json";
             rbLocalSettings.UseVisualStyleBackColor = true;
@@ -179,9 +188,10 @@
             // 
             rbAppSettings.AutoSize = true;
             rbAppSettings.Checked = true;
-            rbAppSettings.Location = new Point(7, 22);
+            rbAppSettings.Location = new Point(8, 29);
+            rbAppSettings.Margin = new Padding(3, 4, 3, 4);
             rbAppSettings.Name = "rbAppSettings";
-            rbAppSettings.Size = new Size(111, 19);
+            rbAppSettings.Size = new Size(137, 24);
             rbAppSettings.TabIndex = 13;
             rbAppSettings.TabStop = true;
             rbAppSettings.Text = "appsettings.json";
@@ -189,22 +199,25 @@
             // 
             // chkbOverrideSettingFile
             // 
+            chkbOverrideSettingFile.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             chkbOverrideSettingFile.AutoSize = true;
             chkbOverrideSettingFile.Checked = true;
             chkbOverrideSettingFile.CheckState = CheckState.Checked;
-            chkbOverrideSettingFile.Location = new Point(289, 70);
+            chkbOverrideSettingFile.Location = new Point(242, 99);
+            chkbOverrideSettingFile.Margin = new Padding(3, 4, 3, 4);
             chkbOverrideSettingFile.Name = "chkbOverrideSettingFile";
-            chkbOverrideSettingFile.Size = new Size(153, 19);
+            chkbOverrideSettingFile.Size = new Size(193, 24);
             chkbOverrideSettingFile.TabIndex = 14;
             chkbOverrideSettingFile.Text = "Override Destination file";
             chkbOverrideSettingFile.UseVisualStyleBackColor = true;
             // 
             // toolStrip1
             // 
+            toolStrip1.ImageScalingSize = new Size(20, 20);
             toolStrip1.Items.AddRange(new ToolStripItem[] { newToolStripButton, toolStripSeparator1, helpToolStripButton });
             toolStrip1.Location = new Point(0, 0);
             toolStrip1.Name = "toolStrip1";
-            toolStrip1.Size = new Size(1139, 25);
+            toolStrip1.Size = new Size(1302, 27);
             toolStrip1.TabIndex = 15;
             toolStrip1.Text = "toolStrip1";
             // 
@@ -214,14 +227,14 @@
             newToolStripButton.Image = (Image)resources.GetObject("newToolStripButton.Image");
             newToolStripButton.ImageTransparentColor = Color.Magenta;
             newToolStripButton.Name = "newToolStripButton";
-            newToolStripButton.Size = new Size(23, 22);
+            newToolStripButton.Size = new Size(29, 24);
             newToolStripButton.Text = "&New";
             newToolStripButton.Click += newToolStripButton_Click;
             // 
             // toolStripSeparator1
             // 
             toolStripSeparator1.Name = "toolStripSeparator1";
-            toolStripSeparator1.Size = new Size(6, 25);
+            toolStripSeparator1.Size = new Size(6, 27);
             // 
             // helpToolStripButton
             // 
@@ -229,16 +242,17 @@
             helpToolStripButton.Image = (Image)resources.GetObject("helpToolStripButton.Image");
             helpToolStripButton.ImageTransparentColor = Color.Magenta;
             helpToolStripButton.Name = "helpToolStripButton";
-            helpToolStripButton.Size = new Size(23, 22);
+            helpToolStripButton.Size = new Size(29, 24);
             helpToolStripButton.Text = "He&lp";
             helpToolStripButton.Click += helpToolStripButton_Click;
             // 
             // btnExportAzure
             // 
             btnExportAzure.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            btnExportAzure.Location = new Point(440, 21);
+            btnExportAzure.Location = new Point(479, 28);
+            btnExportAzure.Margin = new Padding(3, 4, 3, 4);
             btnExportAzure.Name = "btnExportAzure";
-            btnExportAzure.Size = new Size(64, 68);
+            btnExportAzure.Size = new Size(97, 91);
             btnExportAzure.TabIndex = 16;
             btnExportAzure.Text = "Get Azure Settings";
             btnExportAzure.UseVisualStyleBackColor = true;
@@ -249,10 +263,11 @@
             tcInput.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             tcInput.Controls.Add(tpAzure);
             tcInput.Controls.Add(tpConfigFile);
-            tcInput.Location = new Point(6, 21);
+            tcInput.Location = new Point(7, 28);
+            tcInput.Margin = new Padding(3, 4, 3, 4);
             tcInput.Name = "tcInput";
             tcInput.SelectedIndex = 0;
-            tcInput.Size = new Size(518, 124);
+            tcInput.Size = new Size(592, 165);
             tcInput.TabIndex = 17;
             tcInput.SelectedIndexChanged += tcInput_SelectedIndexChanged;
             // 
@@ -263,10 +278,11 @@
             tpAzure.Controls.Add(lblResourceGroupName);
             tpAzure.Controls.Add(tbxResourceGroupName);
             tpAzure.Controls.Add(btnExportAzure);
-            tpAzure.Location = new Point(4, 24);
+            tpAzure.Location = new Point(4, 29);
+            tpAzure.Margin = new Padding(3, 4, 3, 4);
             tpAzure.Name = "tpAzure";
-            tpAzure.Padding = new Padding(3);
-            tpAzure.Size = new Size(510, 96);
+            tpAzure.Padding = new Padding(3, 4, 3, 4);
+            tpAzure.Size = new Size(584, 132);
             tpAzure.TabIndex = 0;
             tpAzure.Text = "Azure";
             tpAzure.UseVisualStyleBackColor = true;
@@ -274,35 +290,37 @@
             // lblWebApp
             // 
             lblWebApp.AutoSize = true;
-            lblWebApp.Location = new Point(6, 48);
+            lblWebApp.Location = new Point(7, 64);
             lblWebApp.Name = "lblWebApp";
-            lblWebApp.Size = new Size(89, 15);
+            lblWebApp.Size = new Size(112, 20);
             lblWebApp.TabIndex = 20;
             lblWebApp.Text = "Web App name";
             // 
             // tbxWebAppName
             // 
             tbxWebAppName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            tbxWebAppName.Location = new Point(6, 66);
+            tbxWebAppName.Location = new Point(7, 88);
+            tbxWebAppName.Margin = new Padding(3, 4, 3, 4);
             tbxWebAppName.Name = "tbxWebAppName";
-            tbxWebAppName.Size = new Size(428, 23);
+            tbxWebAppName.Size = new Size(466, 27);
             tbxWebAppName.TabIndex = 19;
             // 
             // lblResourceGroupName
             // 
             lblResourceGroupName.AutoSize = true;
-            lblResourceGroupName.Location = new Point(6, 3);
+            lblResourceGroupName.Location = new Point(7, 4);
             lblResourceGroupName.Name = "lblResourceGroupName";
-            lblResourceGroupName.Size = new Size(124, 15);
+            lblResourceGroupName.Size = new Size(155, 20);
             lblResourceGroupName.TabIndex = 18;
             lblResourceGroupName.Text = "Resource Group name";
             // 
             // tbxResourceGroupName
             // 
             tbxResourceGroupName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            tbxResourceGroupName.Location = new Point(6, 21);
+            tbxResourceGroupName.Location = new Point(7, 28);
+            tbxResourceGroupName.Margin = new Padding(3, 4, 3, 4);
             tbxResourceGroupName.Name = "tbxResourceGroupName";
-            tbxResourceGroupName.Size = new Size(428, 23);
+            tbxResourceGroupName.Size = new Size(466, 27);
             tbxResourceGroupName.TabIndex = 17;
             // 
             // tpConfigFile
@@ -310,36 +328,39 @@
             tpConfigFile.Controls.Add(label1);
             tpConfigFile.Controls.Add(tbxExportedSettingsFilePath);
             tpConfigFile.Controls.Add(btnBrowseExportedSettings);
-            tpConfigFile.Location = new Point(4, 24);
+            tpConfigFile.Location = new Point(4, 29);
+            tpConfigFile.Margin = new Padding(3, 4, 3, 4);
             tpConfigFile.Name = "tpConfigFile";
-            tpConfigFile.Padding = new Padding(3);
-            tpConfigFile.Size = new Size(510, 96);
+            tpConfigFile.Padding = new Padding(3, 4, 3, 4);
+            tpConfigFile.Size = new Size(584, 132);
             tpConfigFile.TabIndex = 1;
             tpConfigFile.Text = "Exported Setting File";
             tpConfigFile.UseVisualStyleBackColor = true;
             // 
             // gbInput
             // 
-            gbInput.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             gbInput.Controls.Add(tcInput);
-            gbInput.Location = new Point(12, 79);
+            gbInput.Location = new Point(14, 105);
+            gbInput.Margin = new Padding(3, 4, 3, 4);
             gbInput.Name = "gbInput";
-            gbInput.Size = new Size(530, 150);
+            gbInput.Padding = new Padding(3, 4, 3, 4);
+            gbInput.Size = new Size(606, 200);
             gbInput.TabIndex = 18;
             gbInput.TabStop = false;
             gbInput.Text = "Settings Source";
             // 
             // gbDestination
             // 
-            gbDestination.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             gbDestination.Controls.Add(groupBox1);
             gbDestination.Controls.Add(label2);
             gbDestination.Controls.Add(tbxLocalSettingsFilePath);
             gbDestination.Controls.Add(chkbOverrideSettingFile);
             gbDestination.Controls.Add(btnBrowseLocalSettings);
-            gbDestination.Location = new Point(559, 79);
+            gbDestination.Location = new Point(639, 105);
+            gbDestination.Margin = new Padding(3, 4, 3, 4);
             gbDestination.Name = "gbDestination";
-            gbDestination.Size = new Size(448, 150);
+            gbDestination.Padding = new Padding(3, 4, 3, 4);
+            gbDestination.Size = new Size(512, 200);
             gbDestination.TabIndex = 19;
             gbDestination.TabStop = false;
             gbDestination.Text = "Destination File";
@@ -348,9 +369,11 @@
             // 
             groupBox1.Controls.Add(rbAppSettings);
             groupBox1.Controls.Add(rbLocalSettings);
-            groupBox1.Location = new Point(6, 68);
+            groupBox1.Location = new Point(7, 91);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(200, 73);
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(229, 97);
             groupBox1.TabIndex = 21;
             groupBox1.TabStop = false;
             groupBox1.Text = "Dev setting file type";
@@ -359,24 +382,39 @@
             // 
             gbOutput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             gbOutput.Controls.Add(tbxOutput);
-            gbOutput.Location = new Point(14, 235);
+            gbOutput.Location = new Point(16, 313);
+            gbOutput.Margin = new Padding(3, 4, 3, 4);
             gbOutput.Name = "gbOutput";
-            gbOutput.Size = new Size(1101, 501);
+            gbOutput.Padding = new Padding(3, 4, 3, 4);
+            gbOutput.Size = new Size(1258, 668);
             gbOutput.TabIndex = 20;
             gbOutput.TabStop = false;
             gbOutput.Text = "Output";
             // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnMerge);
+            groupBox2.Location = new Point(1158, 105);
+            groupBox2.Margin = new Padding(3, 4, 3, 4);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(3, 4, 3, 4);
+            groupBox2.Size = new Size(130, 200);
+            groupBox2.TabIndex = 21;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Final Step";
+            // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1139, 742);
+            ClientSize = new Size(1302, 989);
+            Controls.Add(groupBox2);
             Controls.Add(gbOutput);
             Controls.Add(gbDestination);
             Controls.Add(gbInput);
             Controls.Add(toolStrip1);
-            Controls.Add(btnMerge);
             Controls.Add(label3);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "MainForm";
             Text = "Settings Merger";
             toolStrip1.ResumeLayout(false);
@@ -393,6 +431,7 @@
             groupBox1.PerformLayout();
             gbOutput.ResumeLayout(false);
             gbOutput.PerformLayout();
+            groupBox2.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -428,5 +467,6 @@
         private TextBox tbxResourceGroupName;
         private GroupBox gbOutput;
         private GroupBox groupBox1;
+        private GroupBox groupBox2;
     }
 }
